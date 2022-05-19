98862231ca66e4286f7e1e154020d8d6fce7f986
java <90934556+javatrainers@users.noreply.github.com>
2022-05-19 10:18:50 -0500

    controlflow

--- src/controlFlow/ControlFlowTest.java
+++ src/controlFlow/ControlFlowTest.java
@@ -1,5 +1,7 @@
 package controlFlow;
 
+import java.util.Scanner;
+
 public class ControlFlowTest {
 	static int balance = 10000;
 	static int regPin=1234;
@@ -16,7 +18,11 @@ public class ControlFlowTest {
 		else
 			System.out.println("invalid pin");
 		
-		boolean x=true;
+		boolean x=true; // statements.
+		// declaration int a;
+		// inititlation int a=10;
+		// assignement a=20;
+		// add(10,20);
 		if(x) // if(true)	
 			System.out.println("hi");
 		
@@ -31,10 +37,81 @@ public class ControlFlowTest {
 			balance=balance-withdrawAmount;
 			System.out.println("collect your cash");
 			System.out.println("your new balance is :"+balance);
-		}else {
+		}
+		else {
 			System.out.println("insufficient fund in your account");
 			System.out.println("please enter amount below :"+balance);
 		}
+		
+		
+		int option=2;
+
+		if(option==1)
+			;// empty block
+		else if(option==2){
+			System.out.println("enter amount to deposit");
+		}
+		else if(option==3){
+			System.out.println("your balance is 1000");
+		}
+		else{ // default block
+			System.out.println("invalid option");
+		}
+		
+		System.out.println("bye bye");
+		
+		
+		if(false);
+			System.out.println("testing");
+		
+		if(false) {
+			
+		}
+			// indepeendent line, not part of above if condition
+			System.out.println("testing");
+		
+			for(   int i=0; i<4   ; i++ ){
+				// print output
+				System.out.println("hi");
+				if(i==2)
+					break;
+				System.out.println("bye");
+			}
+		// sc will allow to take input from screen	
+		Scanner sc=new Scanner(System.in);
+		System.out.println("begin shopping");
+		System.out.println("do you wish to add item to cart? enter true/false");
+		// sc = scanner is a program that will allow to take input from screen
+		// nextBoolean= is a method in scanner program
+		// 				that allows to take boolean input from screen
+		//				boolean input customer entered will be then transfered
+		//				to LHS boolean variable
+		boolean choice=sc.nextBoolean();  
+		while( choice ) {
+			System.out.println("added item to cart");
+			System.out.println("do you wish to add another item to cart? enter true/false");
+			choice=sc.nextBoolean();  
+		}
+		
+		
+		System.out.println("welcome to java bank");
+		do {
+			System.out.println("enter 1 for deposit, 2 for withdraw, 3 for balance");
+			// nextInt = is a method in scanner program
+			// 				that allows to take int input from screen
+			//				int input customer entered will be then transfered
+			//				to LHS int variable
+			int atmOption=sc.nextInt();
+			if(atmOption==1)
+				System.out.println("withdraw complete");
+			else if(atmOption==2)
+				System.out.println("deposit complete");
+			else if (atmOption==3)
+				System.out.println("acct balance is 1000");
+			
+			System.out.println("do you wish to perform another transaction, enter true/false");
+			choice=sc.nextBoolean();
+		}while(choice);
 	}
 
 }
