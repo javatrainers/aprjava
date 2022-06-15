e3052b8f1e673fc2f9f02f7754400d4f1326b7a2
java <90934556+javatrainers@users.noreply.github.com>
2022-06-14 20:44:53 -0500

    file io

---
+++ src/file/PermanantStorage.java
@@ -0,0 +1,31 @@
+package file;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.util.Scanner;
+
+public class PermanantStorage {
+
+	public static void main(String[] args) throws Exception {
+		Product p=new Product();
+		System.out.println( p );
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter prod name");
+		p.name=sc.next();
+		System.out.println("enter prod price");
+		p.price=sc.next();
+		System.out.println(p);
+		
+		
+		FileOutputStream fos=new FileOutputStream("d:/Tutorial/product.obj");
+		ObjectOutputStream os=new ObjectOutputStream(fos);
+		os.writeObject(p); 
+// write object allows to write content of java object 
+//	from java memory (short term) into hard disk storage(long term)
+		
+		os.close();
+		
+	}
+
+}
